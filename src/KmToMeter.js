962335683322eba7eccd0b251a72121df502b801
nnnayy <kny5439@cau.ac.kr>
2022-07-27 15:18:56 +0900

    add km to meter file

---
+++ src/KmToMeter.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { useState } from "react";
+import './App.css';
+
+export function KmToMiles() {
+    const [amount, setAmount] = useState();
+    const [flipped, setFlipped] = useState(false);
+  
+    const onChange =(event)=>{
+        setAmount(event.target.value);
+    };
+    const reset =()=> setAmount(0);
+    const onFlip =()=> {
+        reset();
+        setFlipped((current) => !current);
+    };
+  
+    return (
+      <div>
+        <label htmlFor='km'>Kilometer </label>
+        <input 
+          value={flipped ? amount * 1000 : amount} 
+          id="kilometer" 
+          type="number" 
+          onChange={onChange}
+          disabled={flipped}>
+        </input>
+        <label htmlFor="meter">Meter </label>
+        <input 
+          value={flipped ? amount : amount / 1000}
+          id="meter"
+          type="number"
+          disabled={!flipped}
+          onChange={onChange}>
+        </input>
+        <button onClick={reset}>Reset</button>
+        <button onClick={onFlip}>Flip</button>
+      </div>
+    );
+}
---
+++ src/MinutesTohours.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { useState } from "react";
+import './App.css';
+
+export function MinutesTohours() {
+    const [amount, setAmount] = useState();
+    const [flipped, setFlipped] = useState(false);
+  
+    const onChange =(event)=>{
+        setAmount(event.target.value);
+    };
+    const reset =()=> setAmount(0);
+    const onFlip =()=> {
+        reset();
+        setFlipped((current) => !current);
+    };
+  
+    return (
+      <div>
+        <label htmlFor='minutes'>Minutes </label>
+        <input 
+          value={flipped ? amount * 60 : amount} 
+          id="minutes" 
+          type="number" 
+          onChange={onChange}
+          disabled={flipped}>
+        </input>
+        <label htmlFor="hours">Hours </label>
+        <input 
+          value={flipped ? amount : amount / 60}
+          id="hours"
+          type="number"
+          disabled={!flipped}
+          onChange={onChange}>
+        </input>
+        <button onClick={reset}>Reset</button>
+        <button onClick={onFlip}>Flip</button>
+      </div>
+    );
+}
