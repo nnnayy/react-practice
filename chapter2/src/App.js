7f019acc8e19421f900e426772a7ba310d03781b
nnnayy <kny5439@cau.ac.kr>
2022-07-29 22:32:43 +0900

    change the name(Week1 -> chapter1) & create directory chapter2

---
+++ chapter2/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Btn from './Btn';
+
+function App() {
+  return (
+    <div>
+      <Btn text="Save Changes" fontSize={18} />
+      <Btn text="Continue" />
+    </div>
+  );
+}
+
+export default App;
